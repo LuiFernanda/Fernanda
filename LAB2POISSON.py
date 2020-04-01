9b67cb90a22f9beeaf3d6612e80e5f8439972896
LuiFernanda <61302256+LuiFernanda@users.noreply.github.com>
2020-03-31 18:59:26 -0600

    version 2

--- LAB2POISSON.py
+++ LAB2POISSON.py
@@ -4,5 +4,5 @@ x=int(input("Ingrese el valor de k: "))
 ex=math.exp(-l)
 lan=l**x
 pk=(ex*lan)/math.factorial(x)
-print("El resultado es de: ",pk)
-print("El valor en porcentaje es de: ",pk*100 "%")
+print("El Resultado es: ",pk)
+print("Porcentaje: ",pk*100)
